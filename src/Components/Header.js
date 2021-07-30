da0af7f1f0f10558750c09b48bbbe43a572be7f6
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-07-30 17:56:28 -0400

    creating coffeeverse

---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Header = (props) => {
+    return <h1>Header Component</h1>
+}
+
+export default Header
---
+++ src/Components/Item.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Item = (props) => {
+    return <h1>Item Component</h1>
+}
+
+export default Item
