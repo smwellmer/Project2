679ffadb792037314084c09213c47e663aa18afa
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-08-04 13:10:16 -0400

    wed 110pm

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
-
+import {MdClose} from 'react-icons/md'
+import {FiMenu} from 'react-icons/fi'
 
 
 const Header = (props) => {
+////////////////////////////////////////////////////////////
+/// found a How-To for this Hamburger menu at: 
+// https://ibaslogic.com/how-to-add-hamburger-menu-in-react/
+/////////////////////////////////////////////////////////////
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    const handleToggle = () => {
+        setNavbarOpen(prev => !prev)
+    }
+    const closeMenu = () => {
+        setNavbarOpen(false)
+      }
+
     return <nav className='header'>
-        <Link className="link" to="/">Home</Link>
-        <img className='logo' src='coffeelogo.png' alt='coffeeverse logo'/>
-        <Link className="link" to="MyCart">My Cart</Link>
+        <button className="navbutton"onClick={handleToggle}>{navbarOpen ? (
+            <MdClose style={{ width: "40px", height: "40px" }} />
+            ) : (
+            <FiMenu style={{  width: "40px", height: "40px" }} />
+        )}</button>
+         <ul className={`menuNav ${navbarOpen ? "showMenu" : "" }`}>
+             <Link className="link" to="/" onClick={() => closeMenu()}>Home</Link>
+             <Link className="link" to="MyCart" onClick={() => closeMenu()}>My Cart</Link>
+         </ul>
+        <img className='logo' src='logo-transparent.png' alt='coffeeverse logo'/>
     </nav>
 }
 
 export default Header
+
+// mdclose  color: "#e8e4d9"
+//fi menu color: "black",
--- src/Pages/MyCart.js
+++ src/Pages/MyCart.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Item from '../Components/Item'
 
 const MyCart = (props) => {
-    return <div>
+    return <div className="mycart">
         {props.cart.map((item) => {
             return <Item selected={item} key={item.id} removeFromCart={props.removeFromCart} label="Remove from Cart" />
         })}
